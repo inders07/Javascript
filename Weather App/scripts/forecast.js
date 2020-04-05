ef6f62e88550b8278f91a6430daf4c7a4167455d
Inderjeet Singh <inderjeetsingh9646@gmail.com>
2020-04-05 13:43:17 +0530

    weather app completed

---
+++ Weather App/scripts/forecast.js
@@ -0,0 +1,28 @@
+const key = 'fnGIHL2Sl0RYSlIAeR6bRXnf0pvTGNg5';
+
+// get weather data
+const getWeather = async id => {
+
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+
+    const query = `${id}?apikey=${key}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+};
+
+// get city data
+const getCity = async city => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch(base + query);
+    const data = await response.json();
+
+    return data[0];
+
+};

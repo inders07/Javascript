3d62385798ca71ee81b05907f0338ff773358d33
Inderjeet Singh <inderjeetsingh9646@gmail.com>
2020-04-09 14:58:58 +0530

    UI library completed

---
+++ uiLibrary/src/index.js
@@ -0,0 +1,28 @@
+import Tooltip from './ninjaUI/tooltip';
+import Dropdown from './ninjaUI/dropdown';
+import Tabs from './ninjaUI/tabs';
+import Snackbar from './ninjaUI/snackbar';
+
+// create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'))
+tooltip.init();
+
+// create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown);
+    instance.init();
+})
+
+// create tabs
+const tabs = new Tabs(document.querySelector('.tabs'));
+tabs.init();
+
+// create snackbar
+const snackbar = new Snackbar();
+snackbar.init();
+
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+    snackbar.show('you clicked me :)');
+});
